ae0f85713078d2064b1844644d32a51edb9e8ed7
dnoc <drew.noct@gmail.com>
2016-07-18 12:58:23 -0400

    Refactor, setUp method, green

--- test/java/BabysitterCalculatorTest.java
+++ test/java/BabysitterCalculatorTest.java
@@ -1,11 +1,19 @@
 import main.java.BabysitterCalculator;
+import org.junit.Before;
 import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class BabysitterCalculatorTest {
+    BabysitterCalculator babysitterCalculator;
+
+    @Before
+    public void setUp() {
+        babysitterCalculator = new BabysitterCalculator();
+    }
+
     @Test
     public void whenCalculateIsCalledItReturnsAnInt() {
-        BabysitterCalculator babysitterCalculator = new BabysitterCalculator();
         assertEquals(1, babysitterCalculator.calculate());
     }
+
 }
