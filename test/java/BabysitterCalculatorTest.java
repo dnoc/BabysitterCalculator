fadabfbe3695e0006d2fcf1b9809cd53befdcbed
dnoc <drew.noct@gmail.com>
2016-07-18 13:35:33 -0400

    Limit bed time, red

--- test/java/BabysitterCalculatorTest.java
+++ test/java/BabysitterCalculatorTest.java
@@ -21,4 +21,11 @@ public class BabysitterCalculatorTest {
         assertEquals("Start time cannot be before 5.", babysitterCalculator.calculate(1, 8));
     }
 
+    @Test
+    public void whenBedTimeIsOutOfBoundsCalculateReturnsAMessage() {
+        assertEquals("Bed time must be between 6 and Midnight", babysitterCalculator.calculate(6, 1));
+        assertEquals("Bed time must be between 6 and Midnight", babysitterCalculator.calculate(6, 13));
+        assertEquals("Bed time must be between 6 and Midnight", babysitterCalculator.calculate(5, 13));
+    }
+
 }
