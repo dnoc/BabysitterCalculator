0d71e606a000788b8eda3dfff0d5b52e63cb5722
dnoc <drew.noct@gmail.com>
2016-07-18 13:50:49 -0400

    refactor calculate, created calculatePay method for math, green

--- src/main/java/BabysitterCalculator.java
+++ src/main/java/BabysitterCalculator.java
@@ -8,21 +8,22 @@ public class BabysitterCalculator {
     private static final int MIDNIGHT = 12;
 
     public String calculate(int startTime, int bedTime, int endTime) {
-        int pay;
-
         if (startTime < 5) {
             return "Start time cannot be before 5.";
         } else if ((startTime > bedTime) || (bedTime > MIDNIGHT)) {
             return "Bed time must be between " + startTime + " and Midnight.";
         } else {
-            endTime = endTime + 12;
-
-            int startPay = (bedTime - startTime) * START_TIME_RATE;
-            int bedPay = (MIDNIGHT - bedTime) * BED_TIME_RATE;
-            int endPay = (endTime - MIDNIGHT) * END_TIME_RATE;
-
-            pay = startPay + bedPay + endPay;
+            int pay = calculatePay(startTime, bedTime, endTime);
             return "$" + pay + ".00";
         }
+    }
+
+    private int calculatePay(int startTime, int bedTime, int endTime) {
+        int startPay = (bedTime - startTime) * START_TIME_RATE;
+        int bedPay = (MIDNIGHT - bedTime) * BED_TIME_RATE;
+        int endPay = ((endTime + 12) - MIDNIGHT) * END_TIME_RATE;
+
+        int pay = startPay + bedPay + endPay;
+        return pay;
     }
 }
